fb6cd2673fa906ebc3f69f3cd9443419bb6d2f6d
Yogesh <iamyogess@gmail.com>
2024-02-23 13:51:57 +0545

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./configs/connectDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+//db configure
+connectDB();
+
+app.listen(PORT,()=>{
+    console.log(`Server started at http://localhost:${PORT}`)
+})
