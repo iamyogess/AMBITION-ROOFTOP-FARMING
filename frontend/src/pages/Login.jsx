1d2b6445d3bd74d3d99fdee103d4d392604a8d01
Yogesh <iamyogess@gmail.com>
2024-02-26 12:18:48 +0545

    login register successful

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,27 +1,84 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    const { value } = e.target;
+    setLogin((prev) => ({ ...prev, email: value }));
+  };
+
+  const handlePasswordChange = (e) => {
+    const { value } = e.target;
+    setLogin((prev) => ({ ...prev, password: value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/v1/user/login",
+        login
+      );
+      if (res.data.success === true) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
+    }
+  };
+
   return (
-    <div className='w-full max-container padding-container h-screen flex justify-center items-center  '>
+    <div className="w-full max-container padding-container h-screen flex justify-center items-center  ">
       <div className=" h-[500px] w-[1000px] flex justify-center items-center border-[1px]">
-        <div className='bg-[#2AAA8A] w-[500px] h-full flex flex-col justify-center items-center'>
-          <h1 className='p-4 text-center text-white bold-20'>Welcome friend <br />Login to your account!</h1>
-          <span className='regular-14 text-white '>Create a new account <Link className='regular-14 text-blue-600' to="/signup">Signup</Link></span>
+        <div className="bg-[#2AAA8A] w-[500px] h-full flex flex-col justify-center items-center">
+          <h1 className="p-4 text-center text-white bold-20">
+            Welcome friend <br />
+            Login to your account!
+          </h1>
+          <span className="regular-14 text-white ">
+            Create a new account{" "}
+            <Link className="regular-14 text-blue-600" to="/signup">
+              Sign up
+            </Link>
+          </span>
         </div>
-        <div className='w-[500px] flex flex-col justify-center items-center'>
-          <h2 className='p-3 bold-32 text-[#2AAA8A]'>Login Here</h2>
-          <form action="" className='flex flex-col gap-4'>
-            <input type="text" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your email.' />
-            <input type="text" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your password.' />
-            <button className="py-2 px-6 bg-[#2AAA8A] text-white border border-transparent hover:border-[#2AAA8A]  hover:bg-transparent transition ease-in-out duration-300 hover:text-[#2AAA8A]">
+        <div className="w-[500px] flex flex-col justify-center items-center">
+          <h2 className="p-3 bold-32 text-[#2AAA8A]">Login Here</h2>
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleLogin}
+          >
+            <input
+              type="text"
+              className="py-2 px-6 border border-gray-300 "
+              placeholder="Enter your email."
+              onChange={handleEmailChange}
+            />
+            <input
+              type="password" // Changed type to password
+              className="py-2 px-6 border border-gray-300 "
+              placeholder="Enter your password."
+              onChange={handlePasswordChange} // Changed to handlePasswordChange
+            />
+            <button type="submit" className="py-2 px-6 bg-[#2AAA8A] text-white border border-transparent hover:border-[#2AAA8A]  hover:bg-transparent transition ease-in-out duration-300 hover:text-[#2AAA8A]">
               Login
             </button>
-
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,29 +1,120 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const [register, setRegister] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    houseNo: "",
+    wardNo: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegister((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/v1/user/signup",
+        register
+      );
+      if (res.data.success === true) {
+        toast.success(res.data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      toast.error("Something went wrong!");
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='w-full max-container padding-container h-screen flex justify-center items-center  '>
-      <div className=" h-[500px] w-[1000px] flex justify-center items-center border-[1px]">
-        <div className='bg-[#2AAA8A] w-[500px] h-full flex flex-col justify-center items-center'>
-          <h1 className='p-4 text-center text-white bold-20'>Hello friends <br />Lets create a new account!</h1>
-          <span className='regular-16 text-white'>Already Registered? <Link className='regular-14 text-blue-600' to="/login">Login</Link></span>
+    <div className="w-full max-container padding-container h-screen flex justify-center items-center">
+      <div className="h-[500px] w-[1000px] flex justify-center items-center border-[1px]">
+        <div className="bg-[#2AAA8A] w-[500px] h-full flex flex-col justify-center items-center">
+          <h1 className="p-4 text-center text-white bold-20">
+            Hello friends <br />
+            Let's create a new account!
+          </h1>
+          <span className="regular-16 text-white">
+            Already Registered?{" "}
+            <Link className="regular-14 text-blue-600" to="/login">
+              Login
+            </Link>
+          </span>
         </div>
-        <div className='w-[500px] flex flex-col justify-center items-center'>
-          <h2 className='p-3 bold-32 text-[#2AAA8A]'>Signup Here</h2>
-          <form action="" className='flex flex-col gap-4'>
-            <input type="text" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your full name.' />
-            <input type="text" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your email.' />
-            <input type="text" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your password.' />
-            <input type="number" min="1" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your house number.' />
-            <input type="number" min="1" className='py-2 px-6 border border-gray-300 ' placeholder='Enter your ward number.' />
-            <button className="py-2 px-6 bg-[#2AAA8A] text-white border border-transparent hover:border-[#2AAA8A]  hover:bg-transparent transition ease-in-out duration-300 hover:text-[#2AAA8A]">
-              Signup
+        <div className="w-[500px] flex flex-col justify-center items-center">
+          <h2 className="p-3 bold-32 text-[#2AAA8A]">Sign up Here</h2>
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleRegister}
+          >
+            <input
+              type="text"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Enter your full name."
+              name="fullName"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Enter your email."
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Enter your password."
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Confirm Password."
+              name="confirmPassword"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              min="1"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Enter your house number."
+              name="houseNo"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              min="1"
+              className="py-2 px-6 border border-gray-300"
+              placeholder="Enter your ward number."
+              name="wardNo"
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="py-2 px-6 bg-[#2AAA8A] text-white border border-transparent hover:border-[#2AAA8A]  hover:bg-transparent transition ease-in-out duration-300 hover:text-[#2AAA8A]"
+            >
+              Sign up
             </button>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
